eb359f9eff2e19740a64ac9e5777e5cf9e00e3e2
D4ri4na <159303073+D4ri4na@users.noreply.github.com>
2024-06-04 21:47:53 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int main (){
+    map <int, string> dato ;
+    int fecha;
+    string entrada,evento;
+        cout<<"Ingrese la fecha y el evento : ";
+        cin>> entrada;
+        dato[fecha] = evento;
+        
+}
